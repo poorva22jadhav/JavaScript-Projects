bd258c06455a38e13eea8ec309e0a38c4a357e25
Purva <poorva22jadhav@gmail.com>
2023-06-02 22:21:40 +0530

    concert

---
+++ Concert/printn.java
@@ -0,0 +1,8 @@
+class pattern{
+        public static void main(String args[]){
+                int N=5;
+                for(i=1;i<=N;i++){
+                       System.out.print("*");
+                                   }
+                                       }
+                
